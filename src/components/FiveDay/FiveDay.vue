b4a5a8e7fad5d4d38382f2acbdf64eb1a9e0b69e
Hubert Śmiechowicz <hubert.smiechowicz@hotmail.com>
2024-02-08 21:13:52 +0100

    Small changes

--- src/components/FiveDay/FiveDay.vue
+++ src/components/FiveDay/FiveDay.vue
@@ -1,141 +1,151 @@
 <script setup lang="ts">
-	import { ref, onMounted, computed } from 'vue';
-	import { LineChart } from 'vue-chart-3';
-	import { Chart, registerables } from 'chart.js';
-	import axios, { AxiosResponse } from 'axios';
-	import MainData from '../Template/MainData.vue';
+import { ref, onMounted, computed } from 'vue';
+import { LineChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
+import axios, { AxiosResponse } from 'axios';
+import MainData from '../Template/MainData.vue';
 
-	Chart.register(...registerables);
+Chart.register(...registerables);
 
-	const baseUrl = 'https://meteonowapi.azurewebsites.net/';
-	const cityName = ref<string>('');
-	const temperature = ref<number>(0);
-	const description = ref<string>('');
-	const tempMax = ref<number>(0);
-	const image = ref<string>('');
-	const days = ref<string[]>([]);
-	const temperatureList = ref<number[]>([]);
-	const imageList = ref<string[]>([]);
+const baseUrl = 'https://meteonowapi.azurewebsites.net/';
+const cityName = ref<string>('');
+const temperature = ref<number>(0);
+const description = ref<string>('');
+const tempMax = ref<number>(0);
+const image = ref<string>('');
+const days = ref<string[]>([]);
+const temperatureList = ref<number[]>([]);
+const imageList = ref<string[]>([]);
 
-	const setWeather = (response: AxiosResponse) => {
-		cityName.value = response.data.name;
-		temperature.value = response.data.temp;
-		description.value = response.data.description;
-		image.value = response.data.image;
-	};
+const setWeather = (response: AxiosResponse) => {
+  cityName.value = response.data.name;
+  temperature.value = response.data.temp;
+  description.value = response.data.description;
+  image.value = response.data.image;
+};
 
-	const setChartData = (response: AxiosResponse) => {
-		response.data.forecastDtos.forEach(forecast => {
-			days.value.push(forecast.dt);
-			temperatureList.value.push(forecast.temp);
-			imageList.value.push(forecast.image);
-		});
-	};
+interface ForecastDto {
+  dt: string;
+  temp: number;
+  image: string;
+}
 
-	onMounted(() => {
-		axios
-			.get(`${baseUrl}presentdayforecast?cityName=Warszawa`)
-			.then(response => {
-				setWeather(response);
-			})
-			.then(() => {
-				axios
-					.get(`${baseUrl}fivedaysforecast?cityName=Warszawa`)
-					.then(response => {
-						setChartData(response);
-					});
-			});
-	});
+const setChartData = (
+  response: AxiosResponse<{ forecastDtos: ForecastDto[] }>
+) => {
+  response.data.forecastDtos.forEach((forecast) => {
+    days.value.push(forecast.dt);
+    temperatureList.value.push(forecast.temp);
+    imageList.value.push(forecast.image);
+  });
+};
 
-	const getWeather = (city: string) => {
-		axios
-			.get(`${baseUrl}presentdayforecast?cityName=${city}`)
-			.then(response => {
-				setWeather(response);
-			})
-			.then(() => {
-				days.value = [];
-				temperatureList.value = [];
-				axios
-					.get(`${baseUrl}fivedaysforecast?cityName=${city}`)
-					.then(response => {
-						setChartData(response);
-					});
-			});
-	};
+onMounted(() => {
+  axios
+    .get(`${baseUrl}presentdayforecast?cityName=Warszawa`)
+    .then((response) => {
+      setWeather(response);
+    })
+    .then(() => {
+      axios
+        .get(`${baseUrl}fivedaysforecast?cityName=Warszawa`)
+        .then((response) => {
+          setChartData(response);
+        });
+    });
+});
 
-	const data = computed(() => ({
-		labels: days.value,
-		datasets: [
-			{
-				data: temperatureList.value,
-				borderColor: '#60a5fa',
-			},
-		],
-	}));
+const getWeather = (city: string) => {
+  axios
+    .get(`${baseUrl}presentdayforecast?cityName=${city}`)
+    .then((response) => {
+      setWeather(response);
+    })
+    .then(() => {
+      days.value = [];
+      temperatureList.value = [];
+      axios
+        .get(`${baseUrl}fivedaysforecast?cityName=${city}`)
+        .then((response) => {
+          setChartData(response);
+        });
+    });
+};
 
-	const options = {
-		responsive: true,
-		plugins: {
-			title: {
-				display: true,
-				font: {
-					size: 24,
-				},
-				text: 'Prognoza pogody na pięć dni ',
-				color: '#60a5fa',
-			},
-			legend: {
-				display: false,
-			},
-		},
-		scales: {
-			x: {
-				grid: {
-					display: false,
-				},
-				ticks: {
-					font: {
-						size: 15,
-					},
-					color: '#60a5fa',
-				},
-			},
-			y: {
-				max: 30,
-				min: -20,
-				grid: {
-					display: false,
-				},
-				ticks: {
-					stepSize: 10,
-					font: {
-						size: 15,
-					},
-					color: '#60a5fa',
-				},
-			},
-		},
-	};
+const data = computed(() => ({
+  labels: days.value,
+  datasets: [
+    {
+      data: temperatureList.value,
+      borderColor: '#60a5fa',
+    },
+  ],
+}));
+
+const options = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      font: {
+        size: 24,
+      },
+      text: 'Prognoza pogody na pięć dni ',
+      color: '#60a5fa',
+    },
+    legend: {
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        font: {
+          size: 15,
+        },
+        color: '#60a5fa',
+      },
+    },
+    y: {
+      max: 30,
+      min: -20,
+      grid: {
+        display: false,
+      },
+      ticks: {
+        stepSize: 10,
+        font: {
+          size: 15,
+        },
+        color: '#60a5fa',
+      },
+    },
+  },
+};
 </script>
 
 <template>
-	<div class="h-full w-full text-blue-400 dark:text-slate-50">
-		<MainData
-			:height="'h-3/6 sm:h-2/6 '"
-			:baseUrl="baseUrl"
-			:name="cityName"
-			:temperature="temperature"
-			:description="description"
-			:tempMax="tempMax"
-			:image="image"
-			@get-weather="getWeather" />
+  <div class="h-full w-full text-blue-400 dark:text-slate-50">
+    <MainData
+      :height="'h-3/6 sm:h-2/6 '"
+      :baseUrl="baseUrl"
+      :name="cityName"
+      :temperature="temperature"
+      :description="description"
+      :tempMax="tempMax"
+      :image="image"
+      @get-weather="getWeather"
+    />
 
-		<LineChart
-			:chartData="data"
-			:options="options"
-			class="h-3/6 sm:h-4/6 w-full pb-3 border-b-4 rounded-xl border-slate-50 dark:border-slate-800 bg-blue-100 dark:bg-slate-700" />
-	</div>
+    <LineChart
+      :chartData="data"
+      :options="options"
+      class="h-3/6 sm:h-4/6 w-full pb-3 border-b-4 rounded-xl border-slate-50 dark:border-slate-800 bg-blue-100 dark:bg-slate-700"
+    />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/Template/TemplatePage.vue
+++ src/components/Template/TemplatePage.vue
@@ -1,62 +1,65 @@
 <script setup lang="ts">
-	import { ref, computed } from 'vue';
-	import Brand from './Brand.vue';
-	import ToggleMode from './ToggleMode.vue';
-	import Navigation from './Navigation.vue';
-	import Home from '../Home/Home.vue';
-	import Today from '../Today/Today.vue';
-	import FiveDay from '../FiveDay/FiveDay.vue';
-	import AirPollution from '../AirPollution/AirPollution.vue';
-	import Contact from '../Contact/Contact.vue';
+import { ref, computed } from 'vue';
+import Brand from './Brand.vue';
+import ToggleMode from './ToggleMode.vue';
+import Navigation from './Navigation.vue';
+import Home from '../Home/Home.vue';
+import Today from '../Today/Today.vue';
+import FiveDay from '../FiveDay/FiveDay.vue';
+import AirPollution from '../AirPollution/AirPollution.vue';
+import Contact from '../Contact/Contact.vue';
 
-	const routes = {
-		'/': Home,
-		'/today': Today,
-		'/five-day': FiveDay,
-		'/air-pollution': AirPollution,
-		'/contact': Contact,
-	};
+const routes: Record<string, any> = {
+  '/': Home,
+  '/today': Today,
+  '/five-day': FiveDay,
+  '/air-pollution': AirPollution,
+  '/contact': Contact,
+};
 
-	const currentPath = ref(window.location.hash);
+const currentPath = ref(window.location.hash);
 
-	window.addEventListener('hashchange', () => {
-		currentPath.value = window.location.hash;
-	});
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash;
+});
 
-	const currentView = computed(() => {
-		return routes[currentPath.value.slice(1) || '/'];
-	});
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'];
+});
 
-	const darkMode = ref<string>('');
+const darkMode = ref<string>('');
 
-	const toggleModeFunction = () => {
-		darkMode.value = darkMode.value === 'dark' ? '' : 'dark';
-	};
+const toggleModeFunction = () => {
+  darkMode.value = darkMode.value === 'dark' ? '' : 'dark';
+};
 </script>
 
 <template>
-	<div :class="darkMode" class="font-montserrat">
-		<div
-			id="screen"
-			class="h-screen w-full flex flex-col justify-between bg-slate-50 dark:bg-slate-800">
-			<nav
-				id="nav"
-				class="w-full h-20 flex justify-between items-center md:border-b border-blue-500 dark:border-slate-50">
-				<div class="flex items-center">
-					<Brand />
-					<Navigation />
-				</div>
-				<ToggleMode @toggle-mode="toggleModeFunction" />
-			</nav>
-			<main class="container h-full w-full">
-				<component :is="currentView" />
-			</main>
-			<footer
-				class="h-8 w-full flex justify-center items-center text-sm border-t text-blue-300 border-blue-500 dark:text-slate-300 dark:border-slate-50">
-				Lodz 2024
-			</footer>
-		</div>
-	</div>
+  <div :class="darkMode" class="font-montserrat">
+    <div
+      id="screen"
+      class="h-screen w-full flex flex-col justify-between bg-slate-50 dark:bg-slate-800"
+    >
+      <nav
+        id="nav"
+        class="w-full h-20 flex justify-between items-center md:border-b border-blue-500 dark:border-slate-50"
+      >
+        <div class="flex items-center">
+          <Brand />
+          <Navigation />
+        </div>
+        <ToggleMode @toggle-mode="toggleModeFunction" />
+      </nav>
+      <main class="container h-full w-full">
+        <component :is="currentView" />
+      </main>
+      <footer
+        class="h-8 w-full flex justify-center items-center text-sm border-t text-blue-300 border-blue-500 dark:text-slate-300 dark:border-slate-50"
+      >
+        Lodz 2024
+      </footer>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
